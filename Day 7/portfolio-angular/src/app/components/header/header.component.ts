45b0071acb7e89de7a9755153cc6108d047906ef
Anandu R <ananduraj32@gmail.com>
2021-09-01 15:45:35 +0530

    navbar and header

--- Day 7/portfolio-angular/src/app/components/header/header.component.ts
+++ Day 7/portfolio-angular/src/app/components/header/header.component.ts
@@ -6,7 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
-
+  name = "Anandu R"
+  designation = "Software Engineer"
+  company = "SOTI"
+  profileIcon = "../../../assets/images/profile.jpg"
   constructor() { }
 
   ngOnInit(): void {
