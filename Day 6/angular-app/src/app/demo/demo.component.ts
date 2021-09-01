00a2555287b9c957701ad4ba12768e158a5f73be
Anandu R <ananduraj32@gmail.com>
2021-09-01 17:07:54 +0530

    day 7 complete

--- Day 6/angular-app/src/app/demo/demo.component.ts
+++ Day 6/angular-app/src/app/demo/demo.component.ts
@@ -6,5 +6,18 @@ import { Component } from "@angular/core";
     styleUrls: ['demo.styles.css']
 })
 export default class DemoComponent{
-
+    inputText:string
+    hasError:boolean
+    constructor(){
+        this.inputText = ""
+        this.hasError = false
+    }
+    onchangeText(){
+        if(this.inputText.length>=11){
+            this.hasError = true
+        }
+        else{
+            this.hasError = false
+        }
+    }
 }
