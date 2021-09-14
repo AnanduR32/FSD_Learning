f2d305287e5f2ce7e67728b8118d8c342c692a0a
Anandu R <ananduraj32@gmail.com>
2021-09-14 10:50:27 +0530

    week 4 day 2 checkpoint

---
+++ Week 3/C# basics/EnterpriseIndexed/EnterpriseIndexed/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnterpriseIndexed
+{
+    class Enterprise
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    class EmployeeData
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Gender { get; set; }
+        public int Mobile { get; set; }
+        public int Age { get; set; }
+    }
+
+    class Employees
+    {
+        private List<EmployeeData> EmployeesRecord;
+        Employees()
+        {
+            EmployeesRecord = new List<EmployeeData>();
+            EmployeesRecord.Add(new EmployeeData() { Id = 1, Age = 23, Name="Anandu R" });
+        }
+        public EmployeeData this[int id]
+        {
+            get
+            {
+                return 
+            };
+        }
+    }
+}
