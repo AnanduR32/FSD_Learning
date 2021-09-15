cfc99d3c9e5237017fd6676ee245aa5e608da582
Anandu R <ananduraj32@gmail.com>
2021-09-15 12:20:31 +0530

    Week 4 day 3 checkpoint #1

---
+++ Week 3/C# basics/EnterpriseActionFuncPredicateDelegates/EnterpriseActionFuncPredicateDelegates/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnterpriseActionFuncPredicateDelegates
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee emp = new Employee(1, "Anandu R");
+            Action<string> act = new Action<string>(emp.displayName);
+            Func<int, double> fnc = new Func<int, double>(emp.getSalary);
+            Predicate<string> prd = new Predicate<string>(emp.isName);
+            act("SOTI");
+            Console.WriteLine("Earns Rs.{0}",fnc(127));
+            Console.WriteLine("Name? : {0}",prd("Anandu R"));
+            Console.ReadKey();
+        }
+
+        
+    }
+    
+    class Employee
+    {
+        private int _id;
+        private string _name;
+        public Employee(int id, string name)
+        {
+            this._id = id;
+            this._name = name;
+        }
+        public int Id
+        {
+            get
+            {
+                return this._id;
+            }
+        }
+        public string Name
+        {
+            get
+            {
+                return this._name;
+            }
+        }
+        public double getSalary(int pswd)
+        {
+            if(pswd == 127)
+            {
+                return 1000.0;
+            }
+            else
+            {
+                return 0.0;
+            }
+        }
+        public bool isName(string name)
+        {
+            if(name == this.Name)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public void displayName(string Company)
+        {
+            Console.WriteLine("{0} works at {1}", this.Name, Company);
+        }
+    }
+}
---
+++ Week 3/C# basics/ShapeFeaturesUsingMulticastDelegates/ShapeFeaturesUsingMulticastDelegates/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShapeFeaturesUsingMulticastDelegates
+{
+    public delegate void ComputeFeatures(double Height, double Width);
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Rectangle rect = new Rectangle(1.2, 2.4);
+            ComputeFeatures rectFeatureComputeLogic = new ComputeFeatures(computeLogicArea);
+            rectFeatureComputeLogic += new ComputeFeatures(computeLogicPerimeter);
+            rect.compute(rectFeatureComputeLogic);
+            Console.ReadKey();
+        }
+
+        public static void computeLogicArea(double Height, double Width)
+        {
+            Console.WriteLine("Area: {0}",Width * Height);
+        }
+        public static void computeLogicPerimeter(double Height, double Width)
+        {
+            Console.WriteLine("Perimeter: {0}", 2 * ( Width + Height ));
+        }
+
+    }
+    class Rectangle
+    {
+        private double Height;
+        private double Width;
+
+        public Rectangle(double Height, double Width)
+        {
+            this.Height = Height;
+            this.Width = Width;
+        }
+        public void compute(ComputeFeatures rectFeatureComputeLogic)
+        {
+            rectFeatureComputeLogic(this.Height, this.Width);
+        }
+    }
+}
