80d1747c95c6161ba825682eeaecfd3d7c6f7949
Anandu R <ananduraj32@gmail.com>
2021-09-15 17:10:16 +0530

    week 4 day 3 checkpoint 3 - typesafe collections

---
+++ Week 3/C# basics/EnterpriseCollectionTypesafe/EnterpriseCollectionTypesafe/EmployeeData.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnterpriseCollectionTypesafe
+{
+    public class EmployeeData
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Gender { get; set; }
+        public long Mobile { get; set; }
+        public int Age { get; set; }
+    }
+
+    partial class Employees
+    {
+        public EmployeeData this[int id]
+        {
+            get
+            {
+                return EmployeesRecord.FirstOrDefault(employee => employee.Id == id);
+            }
+            set
+            {
+                EmployeesRecord.FirstOrDefault(employee => employee.Id == id).Id = value.Id;
+                EmployeesRecord.FirstOrDefault(employee => employee.Id == id).Name = value.Name;
+                EmployeesRecord.FirstOrDefault(employee => employee.Id == id).Gender = value.Gender;
+                EmployeesRecord.FirstOrDefault(employee => employee.Id == id).Mobile = value.Mobile;
+            }
+        }
+        public List<EmployeeData> this[string query]
+        {
+            get
+            {
+                if (query == "employee list")
+                {
+                    return this.EmployeesRecord;
+                }
+                else
+                {
+                    return null;
+                }
+            }
+           
+        }
+
+    }
+}
---
+++ Week 3/C# basics/EnterpriseCollectionTypesafe/EnterpriseCollectionTypesafe/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnterpriseCollectionTypesafe
+{
+    class Enterprise
+    {
+        static void Main(string[] args)
+        {
+            Employees EnterpriseEmployee = new Employees();
+            Console.WriteLine("Old : \n{0} {1} {2} {3}", EnterpriseEmployee[1].Id, EnterpriseEmployee[1].Name, EnterpriseEmployee[1].Gender, EnterpriseEmployee[1].Mobile);
+            EnterpriseEmployee[1].Name = "Aquaregis32";
+            Console.WriteLine("New : \n{0} {1} {2} {3}\n", EnterpriseEmployee[1].Id, EnterpriseEmployee[1].Name, EnterpriseEmployee[1].Gender, EnterpriseEmployee[1].Mobile);
+
+            EmployeeData testEmp = new EmployeeData() { Id = 1, Age = 23, Name = "Anandu R", Gender = "Male", Mobile = 7012835996 };
+
+            foreach (var emp in EnterpriseEmployee["employee list"])
+            {
+                Console.WriteLine("{0} {1}", emp.Id, emp.Name);
+            }
+
+            Console.WriteLine("\nTest Employee: \n{0} {1} {2} {3} {4}", testEmp.Id, testEmp.Name, testEmp.Age, testEmp.Gender, testEmp.Mobile);
+
+            Console.WriteLine("Whether test employee in employee records: {0}\n", EnterpriseEmployee["employee list"].Contains(testEmp));
+            Console.WriteLine("Whether employee with name \"Aishwarya Michael\" exists in employee records: {0}\n", EnterpriseEmployee["employee list"].Exists(emp => emp.Name == "Aishwarya Michael"));
+
+
+            Console.WriteLine("\nEmployees with id < 3 : ");
+            foreach (var emp in EnterpriseEmployee["employee list"].FindAll(emp => emp.Id < 3))
+            {
+                Console.WriteLine("{0} {1}", emp.Id, emp.Name);
+            }
+            
+
+            Console.ReadKey();
+        }
+    }
+
+    partial class Employees
+    {
+        private List<EmployeeData> EmployeesRecord;
+        public Employees()
+        {
+            EmployeesRecord = new List<EmployeeData>();
+            EmployeesRecord.Add(new EmployeeData() { Id = 1, Age = 23, Name = "Anandu R", Gender = "Male", Mobile = 7012835996 });
+            EmployeesRecord.Add(new EmployeeData() { Id = 2, Age = 22, Name = "Aishwarya Michael", Gender = "Female", Mobile = 9876543210 });
+            EmployeesRecord.Add(new EmployeeData() { Id = 3, Age = 21, Name = "S Nikhit", Gender = "Female", Mobile = 9876543211 });
+            EmployeesRecord.Add(new EmployeeData() { Id = 4, Age = 22, Name = "Kennith Philips", Gender = "Male", Mobile = 9876543212 });
+        }
+    }
+}
