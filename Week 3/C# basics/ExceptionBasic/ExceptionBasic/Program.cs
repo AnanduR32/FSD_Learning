f1869d1657b2c1b8b4fb0425d413e83f1a4b76f8
Anandu R <ananduraj32@gmail.com>
2021-09-20 13:42:21 +0530

    Exception handling

---
+++ Week 3/C# basics/ExceptionBasic/ExceptionBasic/Program.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace ExceptionBasic
+{
+
+    class UserAlreadyLoggedInException : Exception
+    {
+        public UserAlreadyLoggedInException() : base() { }
+        public UserAlreadyLoggedInException(string msg) : base() { }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamReader stream = null;
+            Auth auth = new Auth();
+
+
+            try
+            {
+                auth.login();
+                Console.WriteLine("Enter an integer number: ");
+                int a = int.Parse(Console.ReadLine());
+
+                stream = new StreamReader(@"../../data/data.txt");
+                Console.WriteLine(stream.ReadToEnd());
+
+               
+            }
+            catch(FileNotFoundException e)
+            {
+                Console.WriteLine("Exception: {0}", e.Message);
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine("Exception: {0}", e.Message);
+            }
+            finally
+            {
+                if(stream != null)
+                {
+                    Console.WriteLine("Closing stream...");
+                    stream.Close();
+                }
+                Console.WriteLine("Execution complete.");
+            }
+
+            Console.ReadKey();
+           
+        }
+    }
+
+    class Auth
+    {
+        private string username;
+        private string password;
+        private bool isLoggedIn;
+        public Auth() {
+            StreamReader stream = null;
+            try
+            {
+                stream = new StreamReader(@"../../data/auth.txt");
+                if(stream.ReadLine() == "user")
+                {
+                    this.isLoggedIn = true;
+                }
+                else
+                {
+                    this.isLoggedIn = false;
+                }
+            }
+            catch(Exception e){
+                Console.WriteLine("Unable to initiate auth service");
+            }
+            finally
+            {
+                stream.Close();
+            }
+            
+        }
+
+        public bool login()
+        {
+            if (isLoggedIn)
+            {
+                throw new UserAlreadyLoggedInException("User already logged in");
+            }
+            Console.WriteLine("Enter login credentials");
+            Console.Write("Username: ");
+            this.username = Console.ReadLine();
+            Console.WriteLine();
+            Console.Write("Password: ");
+            this.password = Console.ReadLine();
+            Console.WriteLine();
+            return this.saveAuth();
+            
+        }
+        private bool saveAuth()
+        {
+            StreamWriter streamWriter = null;
+            try
+            {
+                streamWriter = new StreamWriter(@"../../data/auth.txt");
+                streamWriter.WriteLine(this.username);
+            }
+            catch(FileNotFoundException e)
+            {
+                Console.WriteLine("Exception occured: {0}", e);
+            }
+            finally
+            {
+                streamWriter.Close();
+            }
+            if (streamWriter != null)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
--- Week 3/C# basics/Generics/Generics/Program.cs
+++ Week 3/C# basics/Generics/Generics/Program.cs
@@ -11,6 +11,10 @@ namespace Generics
         static void Main(string[] args)
         {
             Console.WriteLine(AreEqual<int>(1, 2));
+            Solver<int> sObjInt = new Solver<int>(1,1);
+            Console.WriteLine(sObjInt.computeEqual());
+            Solver<string> sObjStr = new Solver<string>("a","b");
+            Console.WriteLine(sObjStr.computeEqual());
             Console.ReadKey();
         }
 
@@ -26,4 +30,26 @@ namespace Generics
             }
         }
     }
+    class Solver<T>
+    {
+        private T a;
+        private T b;
+        public Solver(T a, T b)
+        {
+            this.a = a;
+            this.b = b;
+        }
+        public bool computeEqual()
+        {
+            if (a.Equals(b))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+
 }
