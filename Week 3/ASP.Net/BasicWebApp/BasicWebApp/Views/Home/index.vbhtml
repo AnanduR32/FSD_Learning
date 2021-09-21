d000b067dda1a9fb07629be5fff210e571e90b51
Anandu R <ananduraj32@gmail.com>
2021-09-21 17:38:34 +0530

    week 5 day 1 asp.net MVC

---
+++ Week 3/ASP.Net/BasicWebApp/BasicWebApp/Views/Home/index.vbhtml
@@ -0,0 +1,6 @@
+﻿@Code
+    ViewData("Title") = "index"
+End Code
+
+<h2>index</h2>
+
---
+++ Week 3/ASP.Net/WebAppBasicASP.NetCS/WebAppBasicASP.NetCS/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebAppBasicASP.NetCS.Models;
+
+
+namespace WebAppBasicASP.NetCS.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Details()
+        {
+            Employee employee = new Employee() { Id = 1, Name = "Anandu R", Email = "ananduraj32@gmail.com" };
+            return View(employee);
+        }
+    }
+}
