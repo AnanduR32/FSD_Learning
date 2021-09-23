2f0b9d5769b59829d112580512f84c8477194666
Anandu R <ananduraj32@gmail.com>
2021-09-23 13:06:31 +0530

    Week 5 day 4 - Bookshelf item curator api
    AJAX.. he never even taught

---
+++ Week 3/Web API/BookshelfAPIs/BookshelfAPIs/Controllers/BooksController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using BookshelfAPIs.Models;
+using System.Web.Http.Cors;
+
+namespace BookshelfAPIs.Controllers
+{
+    [EnableCors(origins: "*", headers: "*", methods: "*")]
+    public class BooksController : ApiController
+    {
+        BooksDatabase db = new BooksDatabase();
+        [HttpGet]
+        public List<Book> GetBooks()
+        {
+            return db.Books;
+        }
+    }
+}
---
+++ Week 3/Web API/BookshelfAPIs/BookshelfAPIs/Models/BooksDatabase.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.IO;
+using System.Globalization;
+
+namespace BookshelfAPIs.Models
+{
+    public class BooksDatabase
+    {
+        public List<Book> Books = new List<Book>();
+
+        public BooksDatabase()
+        {
+            LoadData();
+        }
+        private static string path = System.Web.HttpContext.Current.Request.MapPath("../Models/data/books_collection.csv");
+        
+        private static string csvData = System.IO.File.ReadAllText(path);
+        private void LoadData()
+        {
+            foreach (string row in csvData.Split('\n'))
+            {
+                if (!string.IsNullOrEmpty(row))
+                {
+                    Books.Add(new Book
+                    {
+                        ISNB = row.Split(',')[0],
+                        Author = row.Split(',')[1],
+                        Name = row.Split(',')[2],
+                        Date = row.Split(',')[3]
+                    }) ;
+                }
+            }
+            Books.RemoveAt(0);
+        }
+    }
+    public class Book
+    {
+        public string ISNB { get; set; }
+        public string Name { get; set; }
+        public string Author { get; set; }
+        public string Date { get; set; }
+    }
+}
