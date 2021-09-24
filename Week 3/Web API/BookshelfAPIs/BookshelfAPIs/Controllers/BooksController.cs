7432d8a1be0df5cf10e34f7bd68ee48786c7cee3
Anandu R <ananduraj32@gmail.com>
2021-09-24 14:10:23 +0530

    week 5 day 5 - Webapi complete

--- Week 3/Web API/BookshelfAPIs/BookshelfAPIs/Controllers/BooksController.cs
+++ Week 3/Web API/BookshelfAPIs/BookshelfAPIs/Controllers/BooksController.cs
@@ -12,11 +12,29 @@ namespace BookshelfAPIs.Controllers
     [EnableCors(origins: "*", headers: "*", methods: "*")]
     public class BooksController : ApiController
     {
-        BooksDatabase db = new BooksDatabase();
+        BooksDatabase db = BooksDatabase.instantiateDB();
+
         [HttpGet]
         public List<Book> GetBooks()
         {
             return db.Books;
+        }
+        public Book GetBooks(string isbn)
+        {
+            return db.Books.FirstOrDefault(book => book.ISBN == isbn);
+        }
+        
+        [HttpPost]
+        public string PostBook(Book book)
+        {
+            db.PostData(book);
+            return String.Format("Successfully added book! Author: {0} Name: {1} ISBN: {2} Date: {3}", book.Author, book.Name, book.ISBN, book.Date);
+        }
+        [HttpDelete]
+        public string DeleteBook(Book book)
+        {
+            db.DeleteData(book);
+            return String.Format("Deleted book where ISBN == {0}", book.ISBN);
         }
     }
 }
