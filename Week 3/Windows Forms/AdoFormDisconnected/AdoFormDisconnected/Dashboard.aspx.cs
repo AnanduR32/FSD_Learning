3fb30a7b936afff544931a5d98df5c9b607f1316
Anandu R <ananduraj32@gmail.com>
2021-09-29 12:39:19 +0530

    week 6 day 3

---
+++ Week 3/Windows Forms/AdoFormDisconnected/AdoFormDisconnected/Dashboard.aspx.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+
+
+namespace AdoFormDisconnected
+{
+    public partial class Dashboard : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            SqlConnection conn = new SqlConnection(connectionString: ConfigurationManager.ConnectionStrings["BooksDB"].ConnectionString);
+            SqlDataAdapter da = new SqlDataAdapter("select * from books_collection", conn);
+            DataSet ds = new DataSet();
+            da.Fill(ds);
+            GridViewAllBooks.DataSource = ds;
+            GridViewAllBooks.DataBind();
+        }
+    }
+}
